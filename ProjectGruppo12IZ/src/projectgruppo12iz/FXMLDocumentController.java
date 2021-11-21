ae6f8eccde1d9a3c99b604d81a3ae58803b321c0
Utente <Utente@Vito.mshome.net>
2021-11-21 20:23:05 +0100

    First release test windows 2

--- ProjectGruppo12IZ/src/projectgruppo12iz/FXMLDocumentController.java
+++ ProjectGruppo12IZ/src/projectgruppo12iz/FXMLDocumentController.java
@@ -27,7 +27,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World! test 2");
+        label.setText("Hello World! test 3");
     }
     
     @Override
