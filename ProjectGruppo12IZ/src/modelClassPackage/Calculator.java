d3323aaf69cbe0cebc6d8e1c857f6fbc9f4d37d6
t440 <t440@DESKTOP-VSHOVOS.mshome.net>
2021-11-23 13:36:21 +0100

    Addiction method DONE

--- ProjectGruppo12IZ/src/modelClassPackage/Calculator.java
+++ ProjectGruppo12IZ/src/modelClassPackage/Calculator.java
@@ -6,14 +6,31 @@
 
 package modelClassPackage;
 
+import java.math.BigDecimal;
+
 /**
  *
- * @author nando
+ * @author Gruppo 12 IZ
  */
 public class Calculator {
     
+     /**
+     *
+     * executes the addiction of two Complex Numbers and return the sum, if these are not null, otherwise return null.
+     *
+     * @param a its contains the first complex number to add
+     * @param b its contains the second complex number to add
+     * 
+     */
     static ComplexNumber addiction(ComplexNumber a, ComplexNumber b){
-        return null;
+        if(a == null || b == null){
+            System.out.println("Addiction cannot be performed");
+            return null;
+        }
+        BigDecimal realres = a.getReal().add(b.getReal());
+        BigDecimal imagres = a.getImaginary().add(b.getImaginary());
+        ComplexNumber res = new ComplexNumber(realres, imagres);
+        return res;
     }
     static ComplexNumber subtraction(ComplexNumber a, ComplexNumber b){
         return null;
--- ProjectGruppo12IZ/test/modelClassPackage/CalculatorTest.java
+++ ProjectGruppo12IZ/test/modelClassPackage/CalculatorTest.java
@@ -5,6 +5,7 @@
  */
 package modelClassPackage;
 
+import java.math.BigDecimal;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,7 +15,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author nando
+ * @author gruppo 12 IZ
  */
 public class CalculatorTest {
     
@@ -38,20 +39,107 @@ public class CalculatorTest {
     }
 
     /**
-     * Test of addiction method, of class Calculator.
+     * Test of addiction method, of class Calculator, having concordant operands both positive.
      */
     @Test
-    public void testAddiction() {
-        System.out.println("addiction");
-        ComplexNumber a = null;
-        ComplexNumber b = null;
-        ComplexNumber expResult = null;
+    public void testAddictionConcordantPositive() {
+        System.out.println("addiction with concordant positive operands");
+        ComplexNumber a = new ComplexNumber(new BigDecimal("7.2"),new BigDecimal("8.4"));
+        ComplexNumber b = new ComplexNumber("5.68","10.85");
+        ComplexNumber expResult = new ComplexNumber("12.88", "19.25");
         ComplexNumber result = Calculator.addiction(a, b);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
+    
+    /**
+     * Test of addiction method, of class Calculator, having concordant operands both negative.
+     */
+    @Test
+    public void testAddictionConcordantNegative() {
+        System.out.println("addiction with concordant negative operands");
+        ComplexNumber a = new ComplexNumber("-11.30","-4.50");
+        ComplexNumber b = new ComplexNumber("-4.86", "-15.79");
+        ComplexNumber expResult = new ComplexNumber("-16.16", "-20.29");
+        ComplexNumber result = Calculator.addiction(a, b);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of addiction method, of class Calculator, having discordant real part.
+     */
+    @Test
+    public void testAddictionDiscordantReal() {
+        System.out.println("addiction with discordant real part");
+        ComplexNumber a = new ComplexNumber("-12.45","-14.55");
+        ComplexNumber b = new ComplexNumber("9.58", "-1.98");
+        ComplexNumber expResult = new ComplexNumber("-2.87", "-16.53");
+        ComplexNumber result = Calculator.addiction(a, b);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of addiction method, of class Calculator, having discordant imaginary part.
+     */
+    @Test
+    public void testAddictionDiscordantImag() {
+        System.out.println("addiction with discordant imaginary part");
+        ComplexNumber a = new ComplexNumber("2.47", "-14.55");
+        ComplexNumber b = new ComplexNumber("8.53", "13.98");
+        ComplexNumber expResult = new ComplexNumber("11", "-0.57");
+        ComplexNumber result = Calculator.addiction(a, b);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of addiction method, of class Calculator, having only imaginary part.
+     */
+    @Test
+    public void testAddictionNoReal() {
+        System.out.println("addiction with real part equal to 0");
+        ComplexNumber a = new ComplexNumber("0", "4.95");
+        ComplexNumber b = new ComplexNumber("0", "5.05");
+        ComplexNumber expResult = new ComplexNumber("0", "10");
+        ComplexNumber result = Calculator.addiction(a, b);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of addiction method, of class Calculator, having only real part.
+     */
+    @Test
+    public void testAddictionNoImag() {
+        System.out.println("addiction with imaginary part equal to 0");
+        ComplexNumber a = new ComplexNumber("-1.26", "0");
+        ComplexNumber b = new ComplexNumber("3.5", "0");
+        ComplexNumber expResult = new ComplexNumber("2.24", "0");
+        ComplexNumber result = Calculator.addiction(a, b);
+        assertEquals(expResult, result);
+    }
+    
+        
+    /**
+     * Test of addiction method, of class Calculator, having one operand null.
+     */
+    @Test
+    public void testAddictionNull() {
+        System.out.println("addiction with one operand null");
+        ComplexNumber b = new ComplexNumber("3.5", "0");
+        ComplexNumber result = Calculator.addiction(null, b);
+        assertEquals(null, result);
+    }
+    
+        /**
+     * Test of addiction method, of class Calculator, having both operands null.
+     */
+    @Test
+    public void testAddictionBothNull() {
+        System.out.println("addiction with both operands null");
+        ComplexNumber result = Calculator.addiction(null, null);
+        assertEquals(null, result);
+    }
+    
+    
+    
     /**
      * Test of subtraction method, of class Calculator.
      */
