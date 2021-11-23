a764db45385cda07ad14e20aeb90f36e531bc573
alcatraz <ferdisica@icloud.com>
2021-11-23 12:28:03 +0100

    added test for ComplexNumberClass

--- ProjectGruppo12IZ/src/modelClassPackage/ComplexNumber.java
+++ ProjectGruppo12IZ/src/modelClassPackage/ComplexNumber.java
@@ -15,31 +15,130 @@ import java.util.regex.Pattern;
  */
 public class ComplexNumber {
 
-    private final float real;
-    private final float imaginary;
+    private final double real;
+    private final double imaginary;
 
-    public ComplexNumber(float real, float imaginary) {
+    public ComplexNumber(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
 
-
     public static ComplexNumber create(String complexNumber) {
-        return null;
+
+        Pattern complexNumberPattern;
+        double real = 0;
+        double imaginary = 0;
+        Boolean flag = false;
+        if (complexNumber == null){
+            System.out.println("The string is empty");
+            return null;
+        }
+        if (complexNumber.charAt(complexNumber.length()-1) != 'j' && (complexNumber.charAt(complexNumber.length()-1) < '0' && complexNumber.charAt(complexNumber.length()-1) > '9')) {
+            System.out.println("The last elements is not a j or a number");
+            return null;
+        }
+
+        if (complexNumber.charAt(0) == '+' || complexNumber.charAt(0) == '-') {
+            // it contains  -
+            if (complexNumber.charAt(complexNumber.length() - 1) == 'j') {
+                // its contains - j 
+                if (complexNumber.substring(1).contains("+") || complexNumber.substring(1).contains("-")) {
+                    // it contains - x - j
+                    complexNumberPattern = Pattern.compile("{1,}[+-][0-9]{1,}[+-][0-9]{1,}j");
+                    if (complexNumberPattern.matcher(complexNumber).matches()) {
+                        String realSTr = complexNumber.charAt(0) + complexNumber.substring(1).split("[+-]")[0];
+                        real = Double.parseDouble(realSTr);
+                        imaginary = Double.parseDouble(complexNumber.substring(realSTr.length(), complexNumber.length() - 1));
+                    
+                        flag = true;
+                    }
+                } else {
+                    //it contains - j
+                    complexNumberPattern = Pattern.compile("{1,}[+-][0-9]{1,}j");
+                    if (complexNumberPattern.matcher(complexNumber).matches()) {
+                        imaginary = Double.parseDouble(complexNumber.substring(0, complexNumber.length() - 1));
+                        flag = true;
+                    }
+                }
+            } else {
+                complexNumberPattern = Pattern.compile("{1,}[+-][0-9]");
+                if (complexNumberPattern.matcher(complexNumber).matches()) {
+                    real = Double.parseDouble(complexNumber);
+                    flag = true;
+                }
+            }
+        } else {
+            if (complexNumber.charAt(complexNumber.length() - 1) == 'j') {
+                // its contains j 
+                if (complexNumber.substring(1).contains("+") || complexNumber.substring(1).contains("-")) {
+                    // it contains x - j
+                    complexNumberPattern = Pattern.compile("[0-9]{1,}[+-][0-9]{1,}j");
+                    if (complexNumberPattern.matcher(complexNumber).matches()) {
+                        real = Double.parseDouble(complexNumber.substring(0).split("[+-]")[0]);
+                        imaginary = Double.parseDouble(complexNumber.substring(0, complexNumber.length() - 1).split("[+-]")[1]);
+                        flag = true;
+                    }
+                } else {
+                    //it contains j
+                    complexNumberPattern = Pattern.compile("[0-9]{1,}j");
+                    if (complexNumberPattern.matcher(complexNumber).matches()) {
+                        imaginary = Double.parseDouble(complexNumber.substring(0, complexNumber.length() - 1));
+                        flag = true;
+                    }
+                }
+            } else {
+                complexNumberPattern = Pattern.compile("{1,}[0-9]");
+                if (complexNumberPattern.matcher(complexNumber).matches()) {
+                    real = Double.parseDouble(complexNumber);
+                    flag = true;
+                }
+            }
+        }
+        if (flag) {
+            return new ComplexNumber(real, imaginary);
+        } else {
+            System.out.println("Not in Cartesian form");
+            return null;
+        }
     }
 
-    public float getReal() {
+    public double getReal() {
         return real;
     }
 
-    public float getImaginary() {
+    public double getImaginary() {
         return imaginary;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ComplexNumber other = (ComplexNumber) obj;
+        if (Double.doubleToLongBits(this.real) != Double.doubleToLongBits(other.real)) {
+            return false;
+        }
+        return Double.doubleToLongBits(this.imaginary) == Double.doubleToLongBits(other.imaginary);
+    }
+
+
 
     @Override
     public String toString() {
         return "ComplexNumber{" + "real=" + real + ", imaginary=" + imaginary + '}';
     }
 
-   
 }
--- ProjectGruppo12IZ/test/modelClassPackage/ComplexNumberTest.java
+++ ProjectGruppo12IZ/test/modelClassPackage/ComplexNumberTest.java
@@ -39,13 +39,11 @@ public class ComplexNumberTest {
      */
     @Test
     public void testCreate() {
-        System.out.println("create");
-        String complexNumber = "";
-        ComplexNumber expResult = null;
+        System.out.println("create in Cartesian Notation (+x+j)");
+        String complexNumber = "+5+7j";
+        ComplexNumber expResult = new ComplexNumber(5, 7);
         ComplexNumber result = ComplexNumber.create(complexNumber);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -54,9 +52,9 @@ public class ComplexNumberTest {
     @Test
     public void testGetReal() {
         System.out.println("getReal");
-        ComplexNumber instance = new ComplexNumber(5,1);
-        float expResult = 5.0F;
-        float result = instance.getReal();
+        ComplexNumber instance = new ComplexNumber(5, 1);
+        double expResult = 5.0;
+        double result = instance.getReal();
         assertEquals(expResult, result, 0.0);
     }
 
@@ -66,9 +64,9 @@ public class ComplexNumberTest {
     @Test
     public void testGetImaginary() {
         System.out.println("getImaginary");
-        ComplexNumber instance = new ComplexNumber(5,1);
-        float expResult = 1.0F;
-        float result = instance.getImaginary();
+        ComplexNumber instance = new ComplexNumber(5, 1);
+        double expResult = 1.0F;
+        double result = instance.getImaginary();
         assertEquals(expResult, result, 0.0);
     }
 
@@ -78,14 +76,165 @@ public class ComplexNumberTest {
     @Test
     public void testToString() {
         System.out.println("toString");
-        ComplexNumber instance = null;
-        String expResult = "";
+        ComplexNumber instance = new ComplexNumber(5, 1);
+        String expResult = "ComplexNumber{real=5.0, imaginary=1.0}";
         String result = instance.toString();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
+    /**
+     * Test of create method with Cartesian Notation +x-j
+     */
+    @Test
+    public void testCreateCartPlusMinus() {
+        System.out.println("Cartesian Notation (+x-j)");
+        String complexNumber = "+5-7j";
+        ComplexNumber expResult = new ComplexNumber(5, -7);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
 
+    /**
+     * Test of create method with Cartesian Notation -x-j
+     */
+    @Test
+    public void testCreateCartMinusMinus() {
+        System.out.println("Cartesian Notation (-x-j)");
+        String complexNumber = "-5-7j";
+        ComplexNumber expResult = new ComplexNumber(-5, -7);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of create method with Cartesian Notation -x+j
+     */
+    @Test
+    public void testCreateCartMinusPlus() {
+        System.out.println("Cartesian Notation (-x+j)");
+        String complexNumber = "-5+7j";
+        ComplexNumber expResult = new ComplexNumber(-5, +7);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of create method with Cartesian Notation -x
+     */
+    @Test
+    public void testCreateCartMinusZero() {
+        System.out.println("Cartesian Notation (-x)");
+        String complexNumber = "-5";
+        ComplexNumber expResult = new ComplexNumber(-5, 0);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of create method with Cartesian Notation +x
+     */
+    @Test
+    public void testCreateCartPlusZero() {
+        System.out.println("Cartesian Notation (+x)");
+        String complexNumber = "+5";
+        ComplexNumber expResult = new ComplexNumber(5, 0);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of create method with Cartesian Notation +j
+     */
+    @Test
+    public void testCreateCartZeroPlus() {
+        System.out.println("Cartesian Notation (+j)");
+        String complexNumber = "+7j";
+        ComplexNumber expResult = new ComplexNumber(0, 7);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of create method with Cartesian Notation -j
+     */
+    @Test
+    public void testCreateCartZeroMinus() {
+        System.out.println("Cartesian Notation (-j)");
+        String complexNumber = "-7j";
+        ComplexNumber expResult = new ComplexNumber(0, -7);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of create method with Cartesian Notation j
+     */
+    @Test
+    public void testCreateCartZeroNone() {
+        System.out.println("Cartesian Notation (j)");
+        String complexNumber = "7j";
+        ComplexNumber expResult = new ComplexNumber(0, 7);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of create method with Cartesian Notation x
+     */
+    @Test
+    public void testCreateCartNoneZero() {
+        System.out.println("Cartesian Notation (x)");
+        String complexNumber = "5";
+        ComplexNumber expResult = new ComplexNumber(5, 0);
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+    /**
+     * Test of create method with Cartesian Notation x
+     */
+    @Test
+    public void testCreateNotInCart() {
+        System.out.println("Cartesian Notation (j+7)");
+        String complexNumber = "5j+7";
+        ComplexNumber expResult = null;
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+     /**
+     * Test of create method with Cartesian Notation x
+     */
+    @Test
+    public void testCreateNotANumber() {
+        System.out.println("Not a number ");
+        String complexNumber = "test";
+        ComplexNumber expResult = null;
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+    /**
+     * Test of create method with Cartesian Notation x
+     */
+    @Test
+    public void testCreateNotWithJ() {
+        System.out.println("Not with j ");
+        String complexNumber = "5+7i";
+        ComplexNumber expResult = null;
+        ComplexNumber result = ComplexNumber.create(complexNumber);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of equals method, of class ComplexNumber.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        ComplexNumber instance = new ComplexNumber(5,7);
+        ComplexNumber instance2 = new ComplexNumber(5,7);
+        boolean expResult = true;
+        boolean result = instance.equals(instance2);
+        assertEquals(expResult, result);
+    }
     
+
 }
