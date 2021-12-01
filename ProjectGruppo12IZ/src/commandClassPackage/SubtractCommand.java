9e73d1c5ce57b5c2ec37daa4bef6fae77fbc91cb
Cami <Cami@Camilla.lan>
2021-12-01 17:58:14 +0100

    Class SubtractCommand Created and Tested

---
+++ ProjectGruppo12IZ/src/commandClassPackage/SubtractCommand.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commandClassPackage;
+
+import modelClassPackage.Calculator;
+import modelClassPackage.ComplexNumber;
+import modelClassPackage.MyOperandCollection;
+
+/**
+ *
+ * @author Gruppo 12 IZ
+ */
+public class SubtractCommand implements Command{
+    private final MyOperandCollection collector;
+    private final ComplexNumber firstOperand;
+    private final ComplexNumber secondOperand;
+    
+    
+    
+    /**
+     * It create an object of the AddCommand if the collector is not null and 
+     * contains enough element to perform the subtraction operation.
+     * @param collector
+     * @see ComplexNumber, MyOperandCollection
+     */
+    public SubtractCommand(MyOperandCollection collector){
+        assert collector != null;
+        this.collector = collector;
+        ComplexNumber value1 = collector.last();
+        ComplexNumber value2 = collector.secondLast();
+        assert value1 != null && value2 != null;
+        this.firstOperand = value1;
+        this.secondOperand = value2;
+    }
+    
+    
+    /**
+     *It execute the operation of subtraction on the last two inserted operand in the collector removing them,
+     * and pushes the result of the operation as last operand.
+     * @see ComplexNumber, MyOperandCollection
+     */
+    @Override
+    public void execute() {
+        //remove the last two inserted operands
+        collector.remove();
+        collector.remove();
+        //execute the subtraction of the two removed operands
+        ComplexNumber result = Calculator.subtraction(secondOperand, firstOperand);
+        //insert the result of the subtraction in the collection
+        collector.insert(result);
+    }
+    
+    /**
+     *It execute the undo of the addiction operation removing the last inserted element that is the result of the operation,
+     * and pushes the two operands that have been involved in the addiction operation.
+     * @see ComplexNumber, MyOperandCollection
+     */
+    @Override
+    public void undo() {
+        //removes the last operand inserted that is the result of the subtraction
+        collector.remove();
+        //pushes the two elements that have been subtracted
+        collector.insert(secondOperand);
+        collector.insert(firstOperand);
+    }
+    
+}
---
+++ ProjectGruppo12IZ/test/commandClassPackage/SubtractCommandTest.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commandClassPackage;
+
+import modelClassPackage.Calculator;
+import modelClassPackage.ComplexNumber;
+import modelClassPackage.MyOperandCollection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gruppo 12 IZ
+ */
+public class SubtractCommandTest {
+    
+    
+    
+    public SubtractCommandTest() {
+       
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+ 
+
+    /**
+     * Test of execute method, of class SubtractionCommand, checking is pushed the correct result.
+     */
+    @Test
+    public void testExecuteCorrectAddiction() {
+        System.out.println("execute correct addiction");
+        MyOperandCollection collector = new MyOperandCollection(12);
+        ComplexNumber a = new ComplexNumber("-2", "9");
+        ComplexNumber b = new ComplexNumber("4", "-3");
+        collector.insert(a);
+        collector.insert(b);
+        SubtractCommand instance = new SubtractCommand(collector);
+        instance.execute();
+        assertEquals(collector.last(),Calculator.subtraction(a, b));
+    }
+    
+     /**
+     * Test of execute method, of class SubtractCommand, checking the correct size of operands in the collector.
+     */
+    @Test
+    public void testExecuteCorrectSize() {
+        System.out.println("execute correct size");
+        MyOperandCollection collector = new MyOperandCollection(12);
+        ComplexNumber a = new ComplexNumber("-2", "9");
+        ComplexNumber b = new ComplexNumber("4", "-3");
+        collector.insert(a);
+        collector.insert(b);
+        SubtractCommand instance = new SubtractCommand(collector);
+        instance.execute();
+        assertEquals(1, collector.collectionLength());
+    }
+    
+
+    /**
+     * Test of undo method, of class SubtractCommand, checking the operands are correctly re-pushed.
+     */
+    @Test
+    public void testUndoCorrectOperands() {
+        System.out.println("undo correct operands");
+        MyOperandCollection collector = new MyOperandCollection(12);
+        ComplexNumber a = new ComplexNumber("-2", "9");
+        ComplexNumber b = new ComplexNumber("4", "-3");
+        collector.insert(a);
+        collector.insert(b);
+        SubtractCommand instance = new SubtractCommand(collector);
+        instance.execute();
+        instance.undo();
+        assertEquals(b, collector.last());
+        assertEquals(a, collector.secondLast()); 
+    }
+    /**
+     * Test of undo method, of class SubtractCommand, checking the operands are correctly repushed.
+     */
+    @Test
+    public void testUndoCorrectSize() {
+        System.out.println("undo correct size");
+        MyOperandCollection collector = new MyOperandCollection(12);
+        ComplexNumber a = new ComplexNumber("-2", "9");
+        ComplexNumber b = new ComplexNumber("4", "-3");
+        collector.insert(a);
+        collector.insert(b);
+        SubtractCommand instance = new SubtractCommand(collector);
+        instance.execute();
+        instance.undo();
+        assertEquals(2, collector.collectionLength());
+    }
+    
+    /**
+     * Test of null collector
+     */
+    @Test(expected = AssertionError.class)
+    public void  testNull(){
+        SubtractCommand sub = new SubtractCommand(null);
+    }
+    
+    /**
+     * Test of collector without elements
+     */
+    @Test(expected = AssertionError.class)
+    public void  testNullOperands(){
+        MyOperandCollection collector = new MyOperandCollection(12);
+        SubtractCommand sub = new SubtractCommand(collector);
+    }
+    
+}
