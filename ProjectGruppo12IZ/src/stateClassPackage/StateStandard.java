5c25174cff068e660e7ce2dac1734bcdb515f873
Cami <Cami@Camilla.lan>
2021-12-07 15:56:08 +0100

    Class StateStandard implemented

--- ProjectGruppo12IZ/src/stateClassPackage/StateStandard.java
+++ ProjectGruppo12IZ/src/stateClassPackage/StateStandard.java
@@ -97,25 +97,34 @@ public class StateStandard extends State{
     }
 
      /**
-     * It create a new SubtractCommand Operation and calls the method execute on it if it is possible.
-     * If the operation could not be performed, because there are not enough operands for example,
-     * an error message will be shown. 
-     * @see FXMLDocumentController, SubtractCommand
+     * It changes the State of the controller to that of StateVariables,
+     * sets this state into the controller and make visible only the buttons 
+     * necessary to perform operations on variables.
+     * @see FXMLDocumentController
      */
     @Override
     public void setStateVariables() {
         controller.changeButtonText(0, ">x");
         controller.changeButtonText(2, "<x");
         controller.changeButtonText(3, "+x");
-        controller.changeButtonText(3, "-x");
+        controller.changeButtonText(5, "-x");
         controller.hideButton(new int[] {1, 4, 6, 7, 8, 9, 10, 11, 12});
-        
-        
+        controller.setState(new StateVariables(controller)); 
     }
 
+    /**
+     * It changes the State of the controller to that of StateOperations,
+     * sets this state into the controller and make visible only the buttons 
+     * necessary to perform on user defined operations.
+     * @see FXMLDocumentController
+     */
     @Override
     public void setStateOperations() {
-      
+        controller.changeButtonText(0, "DELETE");
+        controller.changeButtonText(1, "SHOW");
+        controller.changeButtonText(2, "EXECUTE");
+        controller.hideButton(new int[] {3, 4, 5, 6, 7, 8, 9, 10, 11});
+        controller.setState(new StateOperations(controller));  
     }
     
     
